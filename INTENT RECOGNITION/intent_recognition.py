0e384af4bbc4f418d23c773bb6503332ddd58aca
rogerbaiges <rogerbaigestrilla@gmail.com>
2024-11-12 18:15:08 +0100

    MOD routes in intent_recognition class

--- INTENT RECOGNITION/intent_recognition.py
+++ INTENT RECOGNITION/intent_recognition.py
@@ -73,13 +73,13 @@ class IntentRecognition:
         """
         Loads the training, validation, and test datasets.
         """
-        train_data = pd.read_csv('baiges/data/train.csv', header=None)
-        test_data = pd.read_csv('baiges/data/test.csv', header=None)
+        train_data = pd.read_csv('../data/train.csv', header=None)
+        test_data = pd.read_csv('../data/test.csv', header=None)
         val_data = train_data.tail(900)
         if self.use_augmented_data:
-            train_data = pd.read_csv('baiges/train_data_augmented.csv', header=None)
+            train_data = pd.read_csv('../train_data_augmented.csv', header=None)
         else:
-            train_data = pd.read_csv('baiges/data/train.csv', header=None, nrows=4078)
+            train_data = pd.read_csv('../data/train.csv', header=None, nrows=4078)
         
         
         self.train_data = train_data
@@ -123,7 +123,7 @@ class IntentRecognition:
         return cleaned_labels_list, cleaned_pad_sequences_list
     
     def _save_results(self, results_dict, file_path, header=None):
-        if self.save_results != None:
+        if self.save_results != None and self.save_results != False:
             if self.save_results == "few" and file_path == "./results/complete_results.csv":
                 pass
             with open(file_path, mode='a', newline='', encoding="utf-8") as file:
